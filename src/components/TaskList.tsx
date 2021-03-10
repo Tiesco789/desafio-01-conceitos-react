2bf7ccd679609108d270e946e0ddda8b874f02ec
Franccesco Antonio <franccesco_@hotmail.com>
2021-03-10 12:11:40 -0300

    Criando funções para adicionar ou remover tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    const tasksWithSameTitle = tasks.find(task => task.title === newTaskTitle);
+    
+    if (!tasksWithSameTitle && newTaskTitle) {
+      const newTask = {
+        id: Math.floor(Math.random() * 10),
+        title: newTaskTitle,
+        isComplete: false
+      }
+      
+      setTasks([...tasks, newTask]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    setTasks(
+      tasks.map(
+        task => task.id === id ? { ...task, isComplete: !task.isComplete } : task
+      )
+    );
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    
+    setTasks(
+      tasks.filter(task => task.id !== id)
+    )
   }
 
   return (
